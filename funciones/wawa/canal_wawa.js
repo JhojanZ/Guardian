17af130410845dc7add71abf6c4a3f3ee603ec94
JhojanZ <j.f.sanchez1011@gmail.com>
2024-11-27 13:32:09 -0500

    se agrego caracteres especiales al REGEX_WAWA

--- funciones/wawa/canal_wawa.js
+++ funciones/wawa/canal_wawa.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const REGEX_WAWA = /^(?:([\u{1F300}-\u{1F6FF}\u{1F900}-\u{1F9FF}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F600}-\u{1F64F}]|<(a)?:\w+:\d+>)*|^[<:@#]\w+$|[wa\sáàäâãåąæā+\\\|"'.:;,\-_¡!¿?]+|\B:[^\s]+(?=\s|$))*$/iu;
-
+const REGEX_WAWA = /^(?:([\u{1F300}-\u{1F6FF}\u{1F900}-\u{1F9FF}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F600}-\u{1F64F}]|<(a)?:\w+:\d+>)*|^[<:@#]\w+$|[WAÀÄÂÃÅĄĀªAÁÀÄÂÃÅĄĀª\s*+\\\|"'.:;,\-~^{}()°/_¡!¿?]+|\B:[^\s]+(?=\s|$))*$/iu;
+                                                                                                                                                                        
 const MENSAJE_WAWA = 
 `---------------------------------------------------------------------------
 **¡Atención! El Guardián ha observado tus actos y decreta lo siguiente:** 
--- funciones/wawa/regex.js
+++ funciones/wawa/regex.js
@@ -9,7 +9,7 @@ Debe Aceptar:
 - rechazar todo lo demas
 */
 
-const REGEX_WAWA = /^(?:([\u{1F300}-\u{1F6FF}\u{1F900}-\u{1F9FF}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F600}-\u{1F64F}]|<(a)?:\w+:\d+>)*|^[<:@#]\w+$|[wa\sáàäâãåąæā+\\\|"'.:;,\-_¡!¿?]+|\B:[^\s]+(?=\s|$))*$/iu;
+const REGEX_WAWA = /^(?:([\u{1F300}-\u{1F6FF}\u{1F900}-\u{1F9FF}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F600}-\u{1F64F}]|<(a)?:\w+:\d+>)*|^[<:@#]\w+$|[WAÀÄÂÃÅĄĀªAÁÀÄÂÃÅĄĀª\s*+\\\|"'.:;,\-~^{}()°/_¡!¿?]+|\B:[^\s]+(?=\s|$))*$/iu;
 
 
 function validarWawa(texto) {
